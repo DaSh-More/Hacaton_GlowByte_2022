1cf160536a6afcdd6884b08a7c5c3c13f98a4aa4
Даниил Шевкунов <danila-763@yandex.ru>
2022-10-18 15:56:55 +0300

    Add payments module

---
+++ payments.py
@@ -0,0 +1,96 @@
+from ftplib import FTP_TLS as FTP
+from datetime import datetime, timedelta
+
+
+def time_segments(start, delta: timedelta, end=datetime.today()):
+    """
+    Возвращает даты начиная с указанной и по конечную с указанным шагом
+
+    Args:
+        start (datetime): Начальная дата
+        delta (timedelta): Шаг
+        end (datetime, optional): Конечная дата. Defaults to datetime.today().
+
+    Yields:
+        datetime: Дата
+    """
+    while start <= end:
+        yield start
+        start += delta
+
+
+def files_names(last_date: datetime) -> list:
+    """
+    Возвращает имена файлов которые надо получить
+    (с последней даты по текущую с шагом 30 минут)
+
+    Args:
+        last_date(datetime): Дата начиная с которой возвращать
+
+    Returns:
+        (list): Список имен файлов
+    """
+    last_date = last_date.replace(second=0, minute=0)
+    dates = []
+    template = '%Y-%m-%d_%H-%M'
+    for date in time_segments(last_date, timedelta(minutes=30)):
+        dates.append(f'payment_{date.strftime(template)}.csv')
+    return dates
+
+
+def get_payments_lists(connect_data: dict, last_date: datetime) -> list:
+    """
+    Данные о плетежах начиная с указанной даты
+
+    Args:
+        connect_data (dict): Данные для подключения к ftp
+            (host, user, passwd)
+        last_date (datetime): Дата начиная с которой надо вернуть данные
+
+    Returns:
+        list: Список с данными о плетежах
+    """
+    files = []
+
+    def add_file(file):
+        files.append(file.decode('utf8'))
+    with FTP(**connect_data) as ftp:
+        ftp.prot_p()
+        ftp.cwd('payments')
+        # Получаем файлы с указанной даты
+        for file_name in files_names(last_date):
+            ftp.retrbinary('RETR '+file_name, add_file)
+        data = []
+    # Добавляем в словарь
+    for file in files:
+        data = csv2json(file, data)
+    return data
+
+
+def csv2json(file, data=[]):
+    """
+    Добавляет записи в словарь
+
+    Args:
+        file (str): Содержимое csv файла
+        data (list, optional): Наполняемый список. Defaults to [].
+
+    Returns:
+        list: Список с добавленными записями
+    """
+    for row in file.splitlines():
+        date, card_num, amount = row.split('\t')
+        date = datetime.strptime(date, '%d.%m.%Y %H:%M:%S')
+        data.append({'date': date, 'card_num': card_num, 'amount': amount})
+    return data
+
+
+def main():
+    import json
+    with open('./connect_config.json') as f:
+        data = json.load(f)['ftp']
+    print(get_payments_lists(data, datetime.now())[0])
+
+
+if __name__ == "__main__":
+    main()
