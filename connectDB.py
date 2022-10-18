9afeccd2e40dea196729cc70bda629f820fe7816
vadim <vadim.t0lstykh@yandex.ru>
2022-10-18 19:50:37 +0300

    fixed connect to db with comment

--- connectDB.py
+++ connectDB.py
@@ -23,10 +23,10 @@ class DataConn:
             raise
 
 
-if __name__ == '__main__':
-    diction = {'dbname': 'taxi', 'user': 'etl_tech_user', 'password': 'etl_tech_user_password',
-               'host': 'de-edu-db.chronosavant.ru'}
-    with DataConn(**diction) as cursor:
-        cursor.execute('SELECT * FROM main.rides LIMIT 10')
-        for i in cursor:
-            print(i)
+# if __name__ == '__main__':
+#     diction = {'dbname': 'taxi', 'user': 'etl_tech_user', 'password': 'etl_tech_user_password',
+#                'host': 'de-edu-db.chronosavant.ru'}
+#     with DataConn(**diction) as cursor:
+#         cursor.execute('SELECT * FROM main.rides LIMIT 10')
+#         for i in cursor:
+#             print(i)
