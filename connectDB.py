3c88cbdb713d2228a23bc9ff715a009169d6e780
Даниил Шевкунов <danila-763@yandex.ru>
2022-10-19 14:09:32 +0300

    Доработал DBconnect, теперь не нужен execute

--- connectDB.py
+++ connectDB.py
@@ -1,20 +1,31 @@
-import datetime
-
 import psycopg2
 
 
 class DataConn:
 
-    def __init__(self, **kwargs):
-        self.db_info = kwargs
+    class Cursor:
+        """
+        Объект курсора, при вызове возвращает текст запроса
+        """
+
+        def __init__(self, cursor):
+            self.cursor = cursor
+
+        def __call__(self, sql):
+            self.cursor.execute(sql)
+            return self.cursor.fetchall()
+
+    def __init__(self, **connect_data):
+        needs_data = ['user', 'password', 'host', 'dbname']
+        self.db_info = {key: value for key, value
+                        in connect_data.items() if key in needs_data}
 
     def __enter__(self):
         """
         Открываем подключение с базой данных.
         """
-        self.conn = psycopg2.connect(dbname=self.db_info['dbname'], user=self.db_info['user'],
-                                     password=self.db_info['password'], host=self.db_info['host'])
-        return self.conn.cursor()
+        self.conn = psycopg2.connect(**self.db_info)
+        return self.Cursor(self.conn.cursor())
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """
@@ -23,33 +34,3 @@ class DataConn:
         self.conn.close()
         if exc_val:
             raise
-
-
-# if __name__ == '__main__':
-#     diction = {'dbname': 'taxi', 'user': 'etl_tech_user', 'password': 'etl_tech_user_password',
-#                'host': 'de-edu-db.chronosavant.ru'}
-#     with DataConn(**diction) as cursor:
-#         cursor.execute('SELECT * FROM main.rides LIMIT 10')
-#         for i in cursor:
-#             print(i)
-
-def get_movement_end_cancel(time):
-    movements = []
-    diction = {'dbname': 'taxi', 'user': 'etl_tech_user', 'password': 'etl_tech_user_password',
-                   'host': 'de-edu-db.chronosavant.ru'}
-    with DataConn(**diction) as cursor:
-        cursor.execute("SELECT * FROM main.movement WHERE event = 'END' OR event = 'CANCEL'")
-        for move in cursor:
-            if move[4] > time:
-                move_pars = [
-                              {
-                              "car_plate_num": move[1],
-                              "ride": move[2],
-                              "event": move[3],
-                              "date": move[4]
-                              }
-                            ]
-                movements.append(move_pars)
-
-        return movements
-
